fb6e11c0c8117826ab89d5a9d54231863c488bcc
nesetaydinn <nesataydin1669@gmail.com>
2022-01-23 09:36:49 +0300

    Add header file template

---
+++ lcd4Bit.h
@@ -0,0 +1,16 @@
+#ifndef LCD_4_BIT_LIB
+#define LCD_4_BIT_LIB
+
+#ifdef __cplusplus
+extern 'C' {
+#endif    
+
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdbool.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
