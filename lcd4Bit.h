85e96a7b748518d6d5402d6d36a8f324dd5ab3a5
nesetaydinn <nesataydin1669@gmail.com>
2022-01-23 09:39:53 +0300

    Add lcd4Bit.h

--- lcd4Bit.h
+++ lcd4Bit.h
@@ -9,6 +9,26 @@ extern 'C' {
 #include "stdint.h"
 #include "stdbool.h"
 
+typedef enum{
+    COLOR_BLACK     = 0x0000,
+    COLOR_WHITE     = 0xFFFF,
+    COLOR_RED       = 0xF800,
+    COLOR_GREEN     = 0x07E0,
+    COLOR_BLUE      = 0x001F,
+    COLOR_MAGENTA   = 0xF81F,
+    COLOR_CYAN      = 0x7FFF,
+    COLOR_YELLOW    = 0xFFE0,
+    COLOR_GRAY      = 0X8430,
+    COLOR_BRED      = 0XF81F,
+    COLOR_GRED      = 0XFFE0,
+    COLOR_GBLUE     = 0X07FF,
+    COLOR_BROWN     = 0XBC40,
+    COLOR_BRRED     = 0XFC07,
+    COLOR_DARKBLUE  = 0X01CF,
+    COLOR_LIGHTBLUE = 0X7D7C
+}colors_enum_t;
+
+
 #ifdef __cplusplus
 }
 #endif
